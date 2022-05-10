35bd9080e8150613c2c3b34f26c436bff8c4dc01
VAbukhovich <victorgusha1@gmail.com>
2022-05-10 16:44:26 +0300

    10.05.2022

---
+++ src/by/academy/Test4/Task1/Task1.java
@@ -0,0 +1,87 @@
+package by.academy.Test4.Task1;
+
+import javax.swing.text.DateFormatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAccessor;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public class Task1 {
+    public static void main(String[] args) throws InterruptedException, ParseException {
+        Year year = new Year(2022);
+        Month month = new Month(4);
+        Day day = new Day(10);
+        Date date = new Date(2022-1900, month.month, day.day);
+        Calendar calendar = new GregorianCalendar(year.year, month.month, day.day);
+      //  Thread.sleep(2500);
+        System.out.println(date.toString());
+        Task1.getWeek(date, calendar);
+
+    }
+
+    public static class Year{
+       private int year;
+        public Year(int year){
+            this.year = year;
+        }
+        public int getYear() {
+            return year;
+        }
+        public void setYear(int year) {
+            this.year = year;
+        }
+    }
+
+    public static class Month{
+        private int month;
+        public Month(int month){
+            this.month = month;
+        }
+        public int getMonth() {
+            return month;
+        }
+        public void setMonth(int month) {
+            this.month = month;
+        }
+    }
+
+    public static class Day {
+        private int day;
+        public Day(int day) {
+            this.day = day;
+        }
+        public int getDay() {
+            return day;
+        }
+        public void setDay(int day) {
+            this.day = day;
+        }
+    }
+
+    public static void getWeek(Date date, Calendar calendar) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E MMMM", Locale.ENGLISH);
+        Date date2 = simpleDateFormat.parse("Tue May");
+       // System.out.println(date2.toString());
+
+        SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("EEEE", Locale.ENGLISH);
+        System.out.println( simpleDateFormat1.format(calendar.getTime()));
+        System.out.println( simpleDateFormat1.format(date));
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("EEEE", Locale.ENGLISH);
+        LocalDate localDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)+1,
+                calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println(localDate.format(dateTimeFormatter));
+
+        System.out.println(calendar.getTime().toString() + "\n" + date.toString());
+    }
+}

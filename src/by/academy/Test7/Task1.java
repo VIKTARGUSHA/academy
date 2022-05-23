dba9bb56b6d2dcad5bb9219ce433a4ac5ce409ff
VAbukhovich <victorgusha1@gmail.com>
2022-05-23 16:58:04 +0300

    16.05.2022

--- src/by/academy/Test7/Task1.java
+++ src/by/academy/Test7/Task1.java
@@ -7,10 +7,18 @@ import java.util.stream.*;
 public class Task1  {
     public static void main(String[] args) {
         Random random = new Random();
-        LongStream ls = random.longs(100);
-        LongStream ls1 = LongStream.range(0, 100).limit(100).filter(o -> o < 90).peek(o -> o = (long)(o *  3.14 - 20));
         long [] ls2 = random.longs(100,0,100).toArray();
-        Arrays.stream(ls2).limit(100).filter(o -> o < 90).peek(o -> o = (long)(o *  3.14 - 20)).sorted(new LongComparator());
-       // random.longs(100,0,100).sorted(Comparator.comparing((Long o) -> o));
+        LongStream lstream = LongStream.range(0,100);
+
+        Map<String, Long> map1 = lstream.peek(o ->  o = (long)(o * 3.14 - 20))
+                .skip(3).limit(100).filter(o -> o < 100).boxed().
+                sorted(Comparator.comparing(o -> o)).collect(Collectors
+                        .toMap(o1 -> o1.toString(), o2 -> o2));
+
+        Map<String, Long> map = Arrays.stream(ls2).skip(3).limit(100).
+                map(o -> o = (long)(o * 3.14 - 20)).boxed().filter(o -> o < 100).sorted((o1,o2) -> o1.compareTo(o2))
+                .collect(Collectors.toMap( o1 -> o1.toString(),  o2 -> o2));// ,(o1,o2) -> o1 ));
+
+        System.out.println(map1.size() + map1.toString() +  "\n" + map.size() + map.toString());
     }
 }

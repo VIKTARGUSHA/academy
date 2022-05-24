42bc39eeb902bf98c451f020b1b97c5c7dcb9e5c
VAbukhovich <victorgusha1@gmail.com>
2022-05-24 16:33:44 +0300

    16.05.2022

--- src/by/academy/Test7/Task1.java
+++ src/by/academy/Test7/Task1.java
@@ -7,18 +7,11 @@ import java.util.stream.*;
 public class Task1  {
     public static void main(String[] args) {
         Random random = new Random();
-        long [] ls2 = random.longs(100,0,100).toArray();
-        LongStream lstream = LongStream.range(0,100);
-
-        Map<String, Long> map1 = lstream.peek(o ->  o = (long)(o * 3.14 - 20))
-                .skip(3).limit(100).filter(o -> o < 100).boxed().
-                sorted(Comparator.comparing(o -> o)).collect(Collectors
-                        .toMap(o1 -> o1.toString(), o2 -> o2));
-
-        Map<String, Long> map = Arrays.stream(ls2).skip(3).limit(100).
-                map(o -> o = (long)(o * 3.14 - 20)).boxed().filter(o -> o < 100).sorted((o1,o2) -> o1.compareTo(o2))
-                .collect(Collectors.toMap( o1 -> o1.toString(),  o2 -> o2));// ,(o1,o2) -> o1 ));
-
-        System.out.println(map1.size() + map1.toString() +  "\n" + map.size() + map.toString());
-    }
+        List <Long> list = random.longs(100, 0, 100)
+                .map(o -> o = (long) (o * 3.14 - 20)).filter(o -> o < 100).boxed()
+                .sorted(Comparator.comparing(o -> o)).toList();
+        System.out.println(list.size() + " " + list.toString());
+        Map<String, Long> map = list.stream().collect(Collectors.toMap(o -> o.toString(), o -> o, (o,o1) -> o, TreeMap::new));
+        System.out.println(map.size() + " " + map.toString());
+     }
 }

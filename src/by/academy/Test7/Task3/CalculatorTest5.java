b1e413d5aaee01afa08f686904758abb1dbc604b
VAbukhovich <victorgusha1@gmail.com>
2022-06-02 09:03:13 +0300

    01.06.2022

--- src/by/academy/Test7/Task3/CalculatorTest5.java
+++ src/by/academy/Test7/Task3/CalculatorTest5.java
@@ -21,7 +21,7 @@ class CalculatorTest5 {
     }
 
     @ParameterizedTest(name = "{index} => a = {0}, b = {1}, c = {2}")
-    @MethodSource("someMethod1")
+    @MethodSource("someMethodSubtract")
     public void subtract(int a, int b, int c){
         Calculator calculator = new Calculator();
         int expected = calculator.subtract(a,b);
@@ -29,21 +29,16 @@ class CalculatorTest5 {
         assertEquals(actual, expected);
     }
 
-    private static ArrayList<int []> someMethod1(){
-        int [] array = {1, 2, 3};
-        int [] array1 = {2, 3, 4};
-        int [] array2 = {3, 4, 5};
-        ArrayList<int []> arrayList = new ArrayList<>();
-        arrayList.add(array2);
-        arrayList.add(array);
-        arrayList.add(array1);
-        return arrayList;
-    }
 
     private static Stream<Arguments> someMethod() {
         return Stream.of(Arguments.of(1, 2, 3),
                 Arguments.of(2, 3, 5),
                 Arguments.of(3, 4, 7)
         );
+    }
+    private static Stream<Arguments> someMethodSubtract(){
+        return Stream.of(Arguments.of(1,3,-2),
+                Arguments.of(3,3,0),
+                Arguments.of(5,7,-2));
     }
 }

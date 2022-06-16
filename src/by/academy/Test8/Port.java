0a87967308ac1f6eb6ff70c941e50e9d5242685f
VAbukhovich <victorgusha1@gmail.com>
2022-06-16 16:59:10 +0300

    01.06.2022

--- src/by/academy/Test8/Port.java
+++ src/by/academy/Test8/Port.java
@@ -3,10 +3,10 @@ package by.academy.Test8;
 public class Port {
 public int x = 1;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Store store = new Store();
-        for (int j = 0; j < 5; j++) {
-
+        for (int j = 0; j < 7; j++) {
+//Thread.sleep(1000);
     Terminal3 terminal = new Terminal3(store);
     Thread terminal3 = new Thread(terminal);
     terminal3.start();
